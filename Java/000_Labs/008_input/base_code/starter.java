6420bed5d4b61ba268f34ff3e2820ec04c2db7dd
DavidHovhannisyan1018 <kryptmez@gmail.com>
2026-09-15 21:44:21 +0000

    update

--- Java/000_Labs/008_input/base_code/starter.java
+++ Java/000_Labs/008_input/base_code/starter.java
@@ -9,6 +9,12 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("First Name?");
+		int number = sc.nextlineInt("Age");
+	    
+ 
+
 	}
 }
